a6fbbdd2ea31b399e93d539463ca2d958a64fc6f
jon <goodeyjon@hotmail.com>
2014-04-04 17:08:29 +0100

    first commit

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,23 @@
+module HomeHelper
+  def feeder
+    # rss = SimpleRSS.parse open('http://feeds.feedburner.com/37signals/beMH')
+    rss = SimpleRSS.parse open('http://feeds.bbci.co.uk/sport/0/football/rss.xml?edition=uk')
+    rss
+  end
+   
+  def blog_feed
+    source = "http://feeds.feedburner.com/37signals/beMH" # url or local file
+    content = "" # raw content of rss feed will be loaded here
+    open(source) do |s| content = s.read end
+    rss = RSS::Parser.parse(content, false)
+     
+    # html = "<ul>"
+    # rss.items.each do |i|
+    # html << "<li><a href='#{i.link}'>#{i.title}</a></li>"
+    # html << "</ul>"
+    # html
+     
+    # end
+  
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feeder::Application.config.secret_token = 'b5649e8894a2938211e9a5d3c0a2c273f25147fbb1021cbf282b57b7259d033c6316bd1f71dab07789d067a4de85831a99bfa387ba4477dbb513688cba3918a8'
